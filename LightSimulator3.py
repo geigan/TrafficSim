42be7245549e432cf14085e458c7a0155b3a5277
EthanWaddle <eaw09c@acu.edu>
2016-02-20 18:46:23 -0600

    Update LightSimulator3.py

--- LightSimulator3.py
+++ LightSimulator3.py
@@ -41,23 +41,9 @@ class Instance(object):
 	#def Clock(z):
 		for x in range(self.TotalCycles):
 			print("Cycle:",x+1)
+			NS.DetermineState(x)
+			EW.DetermineState(x)
 			
-			'''	#if(StartsGreen):
-			if((x)%((self.GreenTime+6)*2) < NS.GreenTime):
-				print(NS.Direction,"is green")
-			elif((x)%((self.GreenTime+6)*2) < NS.GreenTime+NS.YellowTime):
-				print(NS.Direction,"is yellow")
-			else:
-				print(NS.Direction,"is red")
-		#else:
-			if((x)%((self.GreenTime+6)*2) < EW.RedTime):
-				print(EW.Direction,"is red")
-			elif((x)%((self.GreenTime+6)*2) < EW.RedTime+EW.YellowTime):
-				print(EW.Direction,"is green")
-			else:
-				print(EW.Direction,"is yellow")'''
-			#NS.DetermineState(True,(x+1)%((self.GreenTime+6)*2)  )
-			#EW.DetermineState(False,(x+1)%((self.GreenTime+6)*2) )
 		
    #def AdvanceCycle():#to be defined/implemented, advances cycle and triggers the changes to the current state of the lights and lanes.
    #def GetCurrentCycle(): #to be defined/implemented, getter for supplying current cycle to other functions
