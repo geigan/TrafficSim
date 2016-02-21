e467976267fe840affc75eb7ed1511b44fc151ef
EthanWaddle <eaw09c@acu.edu>
2016-02-20 20:12:27 -0600

    Update InstanceSim.py

--- InstanceSim.py
+++ InstanceSim.py
@@ -13,16 +13,14 @@ class Lane(object):
                             self.carqueue[x][0]+=1
                             if self.carqueue[x]==[25,26]:
                                     self.carqueue.pop(0) #once a car's head and tail is clear of the intersection, pop it
-                    elif ( self.PositionIsClear((self.carqueue[x][1])+1) ) and (self.carqueue[x][1] < 20):
+                    elif ( self.PositionIsClear((self.carqueue[x][1]) ) ) and (self.carqueue[x][1] < 20):
                             self.carqueue[x][1]+=1
                             self.carqueue[x][0]+=1
-
                             '''x
                             xxx'''
-                    elif self.PositionIsClear((self.carqueue[x][1])+1) and LightStatus=="green":
+                    elif self.PositionIsClear((self.carqueue[x][1]) ) and LightStatus=="green":
                             self.carqueue[x][1]+=1
                             self.carqueue[x][0]+=1
-
                     else:
                             pass
 		self.spawn()#at the end of the movement turn, roll the dice and see if a car spawns
@@ -48,34 +46,37 @@ class Lane(object):
 		print("Lane created.")
 		self.carqueue=[]
 	def spawn(self):
-		if PositionIsClear(2) and PositionIsClear(1):
+		if self.PositionIsClear(2) and self.PositionIsClear(1):
 			self.carqueue.append([1,2])
 
 	def PositionIsClear(self,location):
 		for x in self.carqueue:
 			for y in x:
 				if y==location:
-					print("Position is not clear.")
+					#print("Position is not clear.")
 					return False
-
+		#print("All clear!")
 		return True
 
 	def MoveCar(self,LightStatus):
 		#cars follow one set of rules with a green light
 		#starting at the head of the queue and moving towards the tail, for each
-		if LightStatus == "Green":
-			for x in reversed(self.carqueue):  #iterate through the queue in descending order,
-										#starting with the frontmost car and ending with the last one
-				if self.carqueue[x][1] > 20:
-					self.carqueue[x][1]+=1
-					self.carqueue[x][0]+=1
-					if self.carqueue[x][1] > 25:
-						self.carqueue.pop() #once a car's head is clear of the intersection, pop it
-				elif PositionIsClear((self.carqueue[x][1])+1):
-					self.carqueue[x][1]+=1
-					self.carqueue[x][0]+=1
-				print("Car:",x)
-			spawn(self.carqueue)
+		for x,item in reversed(list(enumerate(self.carqueue))):  #iterate through the queue in descending order
+			if self.carqueue[x][1] > 20: #once we're past the intersection line, we can keep going
+				self.carqueue[x][1]+=1 #...no matter what.  The light will be yellow anyway.
+				self.carqueue[x][0]+=1
+				if self.carqueue[x][1] > 25:
+					self.carqueue.pop() #once a car's head is clear of the intersection, pop it
+			elif self.PositionIsClear((self.carqueue[x][1])+1) and self.carqueue[x][1] < 20:
+				self.carqueue[x][1]+=1
+				self.carqueue[x][0]+=1
+			elif (LightStatus == "green"):
+				self.carqueue[x][1]+=1
+				self.carqueue[x][0]+=1
+			else:#if the space in front is occupied, we cannot move the car
+				pass#so we do nothing and move on to the car behind it
+			print("Car:",x)
+		self.spawn()
 		print("Cars in lane:",self.carqueue)
 	'''Use the same formula Light uses for determining light status to determine what light status to use
 	when calling MoveCar.  MoveCar determines movement by light status, Iterate determines movement by green time and where we are
